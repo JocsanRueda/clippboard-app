b27496f0b7ecd0f17ad1104b2838875e8085e250
Jocsan Rueda <jocsanelyrueda@gmail.com>
2026-01-04 15:07:43 -0600

    fix: reset showToast state in handledOption and refine TextContentEditor styles

--- src/components/Content-Card.tsx
+++ src/components/Content-Card.tsx
@@ -19,6 +19,9 @@ function ContentCard({ text, type,url, toggleActions, handleMenu , handleDelete 
   const handledOption = (e: React.MouseEvent) => {
     e.stopPropagation();
     handleMenu();
+    if (showToast) {
+      setShowToast(false);
+    }
   };
 
   const handleCopyInternal = () => {
@@ -46,11 +49,11 @@ function ContentCard({ text, type,url, toggleActions, handleMenu , handleDelete 
 
   return (
     <div className="flex flex-row justify-between items-stretch  ">
-      <div className="transition-[border] duration-100 w-full bg-gray-200 dark:bg-secondary py-2 px-2 mx-2 rounded-md hover:shadow-lg flex flex-row justify-between border-width-selected  border-gray-300 dark:border-tertiary-dark hover:border-gray-400 hover:dark:border-tertiary-light  transition-border  gap-2   " onClick={handleCopyInternal}>
+      <div className="transition-[border] duration-100 w-full bg-gray-200 dark:bg-secondary py-2 px-2 mx-2 rounded-md hover:shadow-lg flex flex-row justify-between border-width-selected  border-gray-300 dark:border-tertiary-dark hover:border-gray-400 hover:dark:border-tertiary-light  transition-border  gap-2   "  onClick={handleCopyInternal} >
 
-        {<ContentRenderer type={type} value={newText} url={url} editText={toggleActions.activeEdit} setText={setNewText}  />}
+        {<ContentRenderer type={type} value={newText} url={url} editText={toggleActions.activeEdit} setText={setNewText} />}
 
-        <section className="ml-auto flex flex-col justify-between gap-2">
+        <section className="ml-auto flex flex-col justify-between gap-2" onClick={(e)=>{e.stopPropagation();}}>
 
           <HiOutlineEllipsisHorizontal
             className="text-lg text-gray-600 dark:text-quaternary cursor-pointer hover:dark:text-gray-300 hover:text-gray-900  transition-colors duration-200"
