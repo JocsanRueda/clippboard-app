ab6f26916fd462ac208e4ad0d59981990de42744
Jocsan Rueda <jocsanelyrueda@gmail.com>
2026-01-18 10:22:53 -0600

    feat: update tray icon implementation; add new tray icons and improve menu event handling

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,5 +1,6 @@
 // src/tray.rs
 use tauri::{
+    image::Image,
     menu::{Menu, MenuItem},
     tray::TrayIconBuilder,
     App, Manager, Wry,
@@ -12,22 +13,28 @@ pub fn setup_tray(app: &App<Wry>) -> tauri::Result<()> {
     let hidden_i = MenuItem::with_id(app, "hidden", "Hidden", true, None::<&str>)?;
     let menu = Menu::with_items(app, &[&show_i, &hidden_i])?;
     
+    let icon_bytes = include_bytes!("../icons/Logo-tray-light.png");
+    let icon = image::load_from_memory(icon_bytes).expect("Failed to load tray icon");
+    let rgba = icon.to_rgba8();
+    let (width, height) = rgba.dimensions();
+    let rgba_bytes = rgba.into_raw();
 
     TrayIconBuilder::new()
+        .icon(Image::new_owned(rgba_bytes, width, height))
         .menu(&menu)
         .show_menu_on_left_click(true)
         .on_menu_event(|app, event| match event.id.as_ref() {
-            "show" => {
+            "Open" => {
                 if let Some(window) = app.get_webview_window("main") {
                     show_window_command(window);
                 }
             }
-            "hidden" => {
+            "Close" => {
                 if let Some(window) = app.get_webview_window("main") {
                     hide_window_command(window);
                 }
             }
-            _ => println!("Evento de menú no manejado: {:?}", event.id),
+            _ => println!("Unhandled menu event: {:?}", event.id),
         })
         .build(app)?;
 
